9634d4b3c99805be2f692bb45a2b059e4fc9444a
nostalgebraist <nostalgebraist@gmail.com>
2022-07-20 12:23:07 -0700

    concurrent metrics

--- src/nost/metrics.py
+++ src/nost/metrics.py
@@ -1,9 +1,11 @@
 import os
 from typing import Union
 from pprint import pformat
+from functools import partial
 
 import torch as th
 from tqdm.auto import tqdm, trange
+from tqdm.contrib.concurrent import process_map
 
 from src.nost.generation_config import GenerationRunParams, GenerationRuns, RunDirectory
 from src.nost.compute_mauve_from_package import compute_mauve
@@ -33,20 +35,42 @@ class MetricsComputer:
                 print(v)
                 print(f"\t{self.run_directory.metrics[(p, seed)]['mauve']}")
 
-    def do_remaining_metrics(self, seed, post_run_callback=None, **kwargs):
-        for params in self.metrics_to_do(seed):
-            self.summarize_metrics(seed)
-            self.compute_metrics(params, seed=seed, post_run_callback=post_run_callback, **kwargs)
+    def do_remaining_metrics(self, seed, post_run_callback=None, verbose=True, trialrun=False, n_concurrent=1, **kwargs):
+        if n_concurrent > 1:
+            to_do = list(self.metrics_to_do(seed))
+            handler = partial(
+                self.compute_metrics, seed=seed, post_run_callback=post_run_callback, verbose=verbose,
+                trialrun=trialrun, **kwargs
+            )
+            process_map(handler, to_do, max_workers=n_concurrent)
+        else:
+            for params in self.metrics_to_do(seed):
+                self.summarize_metrics(seed)
+                self.compute_metrics(
+                    params,
+                    seed=seed,
+                    post_run_callback=post_run_callback,
+                    verbose=verbose,
+                    trialrun=trialrun,
+                    **kwargs
+                )
 
-    def compute_metrics(self, params, seed, post_run_callback=None, **kwargs):
-        print(f"computing metrics for {pformat(params.to_dict())}")
+    def compute_metrics(self, params, seed, post_run_callback=None, verbose=True, trialrun=False, **kwargs):
+        def vprint(*args, **kwargs):
+            if verbose:
+                print(*args, **kwargs)
+
+        vprint(f"computing metrics for {pformat(params.to_dict())}")
 
         p_feats = self.run_directory.load_groundtruth_feats(params)
         q_feats = self.run_directory.load_feats(params)
 
         metrics_obj = compute_mauve(p_features=p_feats, q_features=q_feats, seed=seed, **kwargs)
 
-        print(f"mauve = {metrics_obj.mauve}")
+        vprint(f"mauve = {metrics_obj.mauve}")
+
+        if trialrun:
+            return
 
         def _handle(obj):
             if isinstance(obj, list) or isinstance(obj, float):
